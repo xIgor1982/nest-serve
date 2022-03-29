371b643d484b9cb126d5970ba06f1919ee14ea3e
i_swat@mail.ru <i_swat@mail.ru>
2022-03-29 15:11:04 +0300

    Настроен сервер
    Подключен сервер, настроен системный файл .env

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+	controllers: [],
+	providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			// envFilePath: `.${process.env.NODE_ENV}.env`,
+			envFilePath: `.env`,
+		}),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			models: [],
+			autoLoadModels: true,
+		}),
+	],
+})
+export class AppModule {}
