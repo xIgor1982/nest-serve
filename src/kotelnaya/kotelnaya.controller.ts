43f70599a60503a557748c7499f891aef9bf7b1a
i_swat@mail.ru <i_swat@mail.ru>
2022-03-29 18:00:47 +0300

    Сделан контроллер - kotelnaya.controller.ts

--- src/kotelnaya/kotelnaya.controller.ts
+++ src/kotelnaya/kotelnaya.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateKotelnayaDto } from './dto/create-kotelnaya.dto';
+import { KotelnayaService } from './kotelnaya.service';
 
 @Controller('kotelnaya')
-export class KotelnayaController {}
+export class KotelnayaController {
+	constructor (private kotelnayaService: KotelnayaService) {}
+
+	@Post()
+	create(@Body() userDto: CreateKotelnayaDto){
+		return this.kotelnayaService.createKotelnaya(userDto);
+	}
+
+	@Get()
+	getAll() {
+		return this.kotelnayaService.getAllKotelnaya()
+	}
+}
