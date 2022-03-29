960f8b7b9bcbdbc279ff9ecf64bef060d7a18b99
i_swat@mail.ru <i_swat@mail.ru>
2022-03-29 16:57:19 +0300

    Доработаны файлы. dto и service у kotelnaya

---
+++ src/kotelnaya/dto/create-kotelnaya.dto.ts
@@ -0,0 +1,5 @@
+export class CreateKotelnayaDto {
+	readonly kot_name: string;
+	readonly kot_address: string;
+	readonly kot_ip: string;
+}
--- src/kotelnaya/kotelnaya.service.ts
+++ src/kotelnaya/kotelnaya.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateKotelnayaDto } from './dto/create-kotelnaya.dto';
+import { Kotelnaya } from './kotelnaya.model';
 
 @Injectable()
-export class KotelnayaService {}
+export class KotelnayaService {
+
+	constructor(@InjectModel(Kotelnaya) private kotelnayaRepository: typeof Kotelnaya){}
+
+	async createKotelnaya(dto: CreateKotelnayaDto) {
+		const kotelnaya = await this.kotelnayaRepository.create(dto);
+		return kotelnaya;
+	}
+
+	async getAllKotelnaya() {
+		const kotelnaya = await this.kotelnayaRepository.findAll();
+		return kotelnaya;
+	}
+}
